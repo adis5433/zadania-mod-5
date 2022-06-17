93ca2ca6c2dcdaf0b66a4f0cf0af8f9524a2fd91
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-06-17 09:36:52 +0200

    add create_contract func

--- id_cards.py
+++ id_cards.py
@@ -1,8 +1,5 @@
 from faker import Faker
-from faker.providers import company
-from faker.providers import job
-from faker.providers import person
-from faker.providers import phone_number
+
 
 fake=Faker()
 
@@ -13,26 +10,52 @@ class BaseContact:
         self.first_name = first_name
         self.last_name = last_name
         self.mail = mail
+        self.phone_number = phone_number
     def introduce(self):
         print(f"Name: {self.first_name}, last name:{self.last_name} E-mail: {self.mail} company: {self.company}")
     @property
-    def lenght_of_full_name(self):
+    def label_length(self):
         len_of_full_name = len(self.first_name)+len(self.last_name)+1
-        return
+        return len_of_full_name
     def contact(self):
-        print(f"We,re contact with {self.first_name} {self.last_name}  the {self.job} of {self.company} e-mail:{self.mail}")
-    def __repr__(self):
-        return f"Name: {self.first_name}, last name:{self.last_name} E-mail: {self.mail}"
+        return f"We,re calling {self.phone_number} - {self.first_name} {self.last_name},  e-mail:{self.mail}"
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} e-mail:{self.mail} phone number: {self.phone_number} "
 
 
 
-class BussinesContact(BaseContact):
-    def __init__(self, company,job):
-        super.__init__(self,*args, **kwargs)
+class BusinessContact(BaseContact):
+    def __init__(self,company, job, business_phone_number, *args,**kwargs):
+        super().__init__(*args,**kwargs)
         self.company = company
         self.job = job
+        self.business_phone_number = business_phone_number
+    def contact(self):
+        return f"We,re contact with {self.first_name} {self.last_name} business phone number: {self.phone_number} the {self.job} of the company: {self.company} " \
+               f"e-mail:{self.mail}"
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} the {self.job} of the {self.company}, " \
+               f"business phone number: {self.business_phone_number} e-mail:{self.mail}"
 
 
-person_one = 
+def create_contact(type_of_contact,number_of_contacts):
+
+    match type_of_contact.lower():
+        case  "basic":
+            for _ in range(number_of_contacts):
+                print((BaseContact(fake.first_name(),fake.last_name(), fake.email(), fake.phone_number())))
+        case "business":
+            for _ in range(number_of_contacts):
+                print((BusinessContact(fake.company(),fake.job(),fake.phone_number(),
+                        fake.first_name(),fake.last_name(),fake.email(), fake.phone_number())))
+        case '_':
+            print("Wrong choice please choose from 'basic' or 'business'")
+
+
+
+
+
+
+create_contact("business",3)
 
 
