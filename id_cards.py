7fd57bc6f25081b541b71422cc9e835cd564d16b
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-06-27 22:37:58 +0200

    add lastest ver

--- id_cards.py
+++ id_cards.py
@@ -11,14 +11,21 @@ class BaseContact:
         self.last_name = last_name
         self.mail = mail
         self.phone_number = phone_number
+
+
     def introduce(self):
         print(f"Name: {self.first_name}, last name:{self.last_name} E-mail: {self.mail} company: {self.company}")
+
+
     @property
     def label_length(self):
         len_of_full_name = len(self.first_name)+len(self.last_name)+1
         return len_of_full_name
     def contact(self):
         return f"We,re calling {self.phone_number} - {self.first_name} {self.last_name},  e-mail:{self.mail}"
+    @property
+
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} e-mail:{self.mail} phone number: {self.phone_number} "
 
@@ -30,9 +37,13 @@ class BusinessContact(BaseContact):
         self.company = company
         self.job = job
         self.business_phone_number = business_phone_number
+
+
     def contact(self):
         return f"We,re contact with {self.first_name} {self.last_name} business phone number: {self.phone_number} the {self.job} of the company: {self.company} " \
                f"e-mail:{self.mail}"
+
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} the {self.job} of the {self.company}, " \
                f"business phone number: {self.business_phone_number} e-mail:{self.mail}"
@@ -44,30 +55,45 @@ def function_performance(func):
     def time_of_one_performance():
         total_performance_time = 0
         start_performance = time.perf_counter()
-        func()
+        print(func())
         end_performance = time.perf_counter()
         total_performance_time = total_performance_time + (end_performance - start_performance)
-        print(total_performance_time)
+        print (total_performance_time)
     return time_of_one_performance
 
 
 
-def create_contact(type_of_contact : str,number_of_contacts : int):
+def create_contact(type_of_contact : str):
+    list_of_contacts = []
     match type_of_contact.lower():
         case  "basic":
-            for _ in range(number_of_contacts):
-                print((BaseContact(fake.first_name(),fake.last_name(), fake.email(), fake.phone_number())))
-        case "business":
-            for _ in range(number_of_contacts):
-                print((BusinessContact(fake.company(),fake.job(),fake.phone_number(),
-                        fake.first_name(),fake.last_name(),fake.email(), fake.phone_number())))
+                contact = BaseContact(fake.first_name(),fake.last_name(), fake.email(), fake.phone_number())
+        case  "business":
+                contact = BusinessContact(fake.company(),fake.job(),fake.phone_number(),
+                        fake.first_name(),fake.last_name(),fake.email(), fake.phone_number())
         case '_':
-            print("Wrong choice please choose from 'basic' or 'business'")
+            return "Wrong choice please choose from 'basic' or 'business'"
+    return contact
 
 
 
-@function_performance
-def thousend_contacts():
-    return create_contact("basic",1000)
+first_id = BusinessContact(fake.company(),fake.job(),fake.phone_number(),
+                        fake.first_name(),fake.last_name(),fake.email(), fake.phone_number())
 
-thousend_contacts()
+print(type(first_id))
+
+
+print(create_contact("business"))
+
+
+
+
+contacts = [create_contact("basic") for _ in range(1000)]
+print (contacts)
+
+
+
+
+
+
+#styl dwie linij
